2cc97248898258761dc2a7e382f1b5276ac8ee1b
César Augusto <cesar.augusto@seoze.com>
2021-05-20 11:19:26 -0300

    vvv

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 function App(props) {
   return (
     <div>
-      <h1>hi thereedddd</h1>
+      <h1>hi thereevvvdddd</h1>
       <hr />
       <MarketingApp />
     </div>
