b0b01dd2ae2e9ba635ea1680f09d8501253c6ab8
César Augusto <cesar.augusto@seoze.com>
2021-05-24 13:27:39 -0300

    Randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,24 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co"
+})
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
 
+
+    <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+      <Header />
       <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
+
   );
 }
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
 export default function App() {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
