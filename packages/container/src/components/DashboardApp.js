93230e393f301784f3ece3abdc0693082ccf3523
César Augusto <cesar.augusto@seoze.com>
2021-05-30 22:39:05 -0300

    Rename the dashboard compo in container

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { mount } from 'dashboard/DashboardApp';
 
-export default function AuthApp() {
+export default function DashboardApp() {
   const ref = useRef(null);
 
   useEffect(() => {
